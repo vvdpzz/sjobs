9e2effa82336f01780edce3719c769deca308399
Zheng-Yu Chen <vvdpzz@gmail.com>
2011-08-31 16:35:50 +0800

    init commit for rails 3.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sjobs::Application.config.secret_token = '6053ebc4d1a1f017a9a872ee34607472b9f8e41c9e3a79185db1fcf3c1297f80489d9ca732459a98eac0bd8d2dc54fd64b3855f282de890d4e14e8f2df77bce9'
