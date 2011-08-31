aa58b6f47b844bac2e96b5dc1ad5d7dd1f697c12
vvdpzz <vvdpzz@gmail.com>
2011-08-31 17:30:52 +0800

    devise added & realname is optional

--- Gemfile
+++ Gemfile
@@ -5,9 +5,17 @@ gem 'rails', '3.1.0'
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
+gem 'SystemTimer'
+
 gem 'mysql2'
 
 gem 'json'
+
+gem 'devise'
+
+gem 'redis'
+
+gem 'resque'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :remember_me
+  
+  attr_accessible :login, :realname, :username, :credit, :money, :about_me
+  
+  attr_accessor :login
+  
+  before_create :create_login
+  
+  protected
+
+  def create_login
+    email = self.email.split(/@/)
+    login_taken = User.where(:username => email[0]).first
+    self.username = email[0] unless login_taken
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  end
+end
