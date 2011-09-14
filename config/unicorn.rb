c8a520fb84c1ccafba31af8fc818082c294c90bb
vvdpzz <vvdpzz@gmail.com>
2011-09-14 11:48:18 +0800

    unicorn config rb

---
+++ config/unicorn.rb
@@ -0,0 +1,18 @@
+#usage:
+#Start:
+#     $ unicorn_rails -c config/unicorn.rb -D
+#Monitor:
+#     $ pgrep -lf unicorn_rails
+#Stop:
+#     $ killall unicorn_rails
+worker_processes 6
+preload_app true
+timeout 30
+listen 3000
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
+
+stderr_path "#{Rails.root}/log/unicorn.log"
+stdout_path "#{Rails.root}/log/unicorn.log"
